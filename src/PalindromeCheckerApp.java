48e11cf9b0fedf2c33343adaf02672273d6d7dae
Kanishk Jaiswal <jaiswal.kanishk.07@gmail.com>
2026-03-25 15:21:49 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -16,3 +16,25 @@ class UseCase1PalindromeCheckerApp {
         System.out.println("System initialized successfully.");
     }
 }
+
+
+//UC2: Hardcoded Palindrome
+class UseCase2PalindromeCheckerApp {
+    public static void main(String[] args) {
+        // Preserving UC1 Welcome
+        System.out.println("Welcome to Palindrome Checker (UC2)");
+
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    }
+}
