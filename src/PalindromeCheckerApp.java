ef075a004a303ba2f117f5c28b80e9168bcc987d
Kanishk Jaiswal <jaiswal.kanishk.07@gmail.com>
2026-03-25 16:49:23 +0530

    UC11: Object-Oriented Palindrome Service

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -255,3 +255,36 @@ class UseCase10PalindromeCheckerApp {
         System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
+
+
+// UC11: Object-Oriented Palindrome Service
+class UseCase11PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        String input = "racecar";
+
+        PalindromeService service = new PalindromeService();
+
+        boolean isPalindrome = service.checkPalindrome(input);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+
+    static class PalindromeService {
+
+        public boolean checkPalindrome(String input) {
+            int start = 0;
+            int end = input.length() - 1;
+
+            while (start < end) {
+                if (input.charAt(start) != input.charAt(end)) {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+            return true;
+        }
+    }
+}
