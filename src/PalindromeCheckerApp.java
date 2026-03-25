5d138dfbf2a6c170dee3da12d7f6ff590e079d55
Kanishk Jaiswal <jaiswal.kanishk.07@gmail.com>
2026-03-25 15:48:05 +0530

    UC5: Stack Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,6 @@
+import java.util.Stack; //UC5
+
+
 // Base application setup
 public class PalindromeCheckerApp {
     public static void main(String[] args){
@@ -74,6 +77,32 @@ class UseCase4PalindromeCheckerApp {
             }
             start++;
             end--;
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
+
+
+// UC5: Stack Based Palindrome Checker
+class UseCase5PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String input = "noon";
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        boolean isPalindrome = true;
+
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
         }
 
         System.out.println("Input : " + input);
