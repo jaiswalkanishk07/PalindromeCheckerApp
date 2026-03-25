e02acec581529c62621e9c85201182ff0ba9623e
Kanishk Jaiswal <jaiswal.kanishk.07@gmail.com>
2026-03-25 16:03:18 +0530

    UC8: Linked List Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -173,3 +173,29 @@ class UseCase7PalindromeCheckerApp {
         System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
+
+
+// UC8: Linked List Based Palindrome Checker
+class UseCase8PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String input = "level";
+
+        LinkedList<Character> list = new LinkedList<>();
+
+        for (char c : input.toCharArray()) {
+            list.add(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (list.size() > 1) {
+            if (!list.removeFirst().equals(list.removeLast())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
